77245a833752af5c56764cd02ed76f65afb87326
Gerson Silva <gerson-gs7@hotmail.com>
2025-11-23 01:30:12 -0300

    feat: adicionando menu com subitens na toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [SharedModule, MatToolbarModule],
+  imports: [SharedModule, MatToolbarModule, MatDividerModule, MatMenuModule],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {
-
-}
+export class ToolbarComponent {}
